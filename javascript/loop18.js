ba4bb1f1279a6f1cfcca3dab55ac167dffe4a2d5
Hitesh2500 <137460224+Hitesh2500@users.noreply.github.com>
2026-06-22 10:23:13 +0530

    l

---
+++ javascript/loop18.js
@@ -0,0 +1,39 @@
+// for( let i=0;i<10;i++){
+//     console.log("HITESH PRAJAPATI")
+// }
+
+
+// // TABLE 4
+// for( let i=1;i<11;i++){
+//     console.log(`4 X ${i} = ${4*i}`)
+// }
+
+
+// day19
+// let i=0;
+// while(i<5)
+// {
+//     console.log("hello")
+//     i++;
+// }
+
+// do while
+// let i=0
+// do{
+//     console.log("hello")
+//     i++
+// }while(i<5);
+
+// s="hitesh kumar prajapati"
+// for (let k of s){
+//     console.log(k) 
+// }
+// let a="hitesh kumar prajapati"
+// let v=["a","e","i","o","u"]
+// let i=0
+// for(let k of a){
+//     if(v.includes(k)){
+//       i++;
+//        }
+// }console.log(i);
+
