55edfe1a6a37b4da9d60ae77c6b2b3a5a3285f38
Hitesh2500 <137460224+Hitesh2500@users.noreply.github.com>
2026-06-22 10:21:52 +0530

    g

--- javascript/javas16.js
+++ javascript/javas16.js
@@ -13,7 +13,89 @@
 
 // 
 
-let a="hsagug"
+// let a="hsagug"
+// let student={
+//     name:"hitesh",
+//     age:"20",
+//     phone:9672903069
 
+// }
+// console.log(student)
 
+// let product={
+//     pname:"shoes",
+//     price:1000,
+//     discount:"30% Off"
+
+// }
+// console.log(product)
+
+// day17
+
+// ARITHMETIC OPERATORS
+// let a=2
+// let b=6
+
+// console.log(a+b)
+// console.log(a%b)
+// console.log(a**3)
+// console.log(a++)
+// console.log(++a)
+// a%=3
+// console.log(a)
+// console.log(a>=b)
+// console.log(a!=b)
+// console.log(a===b)
+// console.log()
+
+// logical  operator
+
+// let a=14
+// let b=14
+// let c=76
+
+// console.log(a==b && b<=c)
+// console.log(a!=b || b>=c)
+// console.log(!(a=b)) 
+
+// CONDITION STATEment
+// example1
+//  let age=20
+//  if(age>=18){
+//     console.log("vote")
+//  }
+//  else{
+//     console.log("not vote")
+//  }
+
+// example2 greatter between two
+// let a=prompt("enter")
+// let b=67
+// if(a<b){
+//     console.log("b is greater than a")
+// }
+// else{
+//     console.log("a is greater than b")
+    
+// }
+
+// // greater between 3 no.
+// let a=3400
+// let b=67888
+// let c=690
+// if(b>a && b>c){
+//     console.log(b)
+// }
+// else if(a>b && a>c){
+//     console.log(a)
+// }
+// else if(c>a && c>b){
+//     console.log(c)
+// }
+
+// day18
+// ternary operator
+let age=67
+let ans=age>=18 ? "vote" : "not vote"
+console.log(ans)
 
